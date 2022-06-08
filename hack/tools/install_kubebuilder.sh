cb4dc7449131fa234752ba9f172c1d9e2b0bc689
Raviv Schaffer <raviv.schaffer@ibm.com>
2022-06-08 17:46:42 +0300

    Added version check when installing tools
    Signed-off-by: Raviv Schaffer <raviv.schaffer@ibm.com>

--- hack/tools/install_kubebuilder.sh
+++ hack/tools/install_kubebuilder.sh
@@ -4,13 +4,12 @@
 
 source ./common.sh
 
-header_text "Checking for bin/etcd"
-[[ -f bin/etcd ]] && exit 0
 
-header_text "Installing bin/etcd"
+header_text "Checking for bin/etcd, bin/kube-apiserver and bin/kubectl ${K8S_VERSION}"
+[[ -f bin/etcd && -f bin/kubectl && -f bin/kube-apiserver && `bin/kubectl version -o=yaml 2> /dev/null | bin/yq e '.clientVersion.gitVersion' -` == "v${K8S_VERSION}" ]] && exit 0
+header_text "Installing bin/etcd, bin/kube-apiserver and bin/kubectl ${K8S_VERSION}"
+
 mkdir -p ./bin
-
-K8S_VERSION=1.22.1
 curl -sSLo envtest-bins.tar.gz "https://storage.googleapis.com/kubebuilder-tools/kubebuilder-tools-${K8S_VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
 
 tar -zvxf envtest-bins.tar.gz
